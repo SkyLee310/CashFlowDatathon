b9b443d1c83c017cb404578b6bfccdac418e131c
SkyLee310 <leeskyxuan@gmail.com>
2025-12-19 14:18:53 +0800

    Pstng Datetime

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import openpyxl
 
-db=pd.read_excel("Datathon Dataset.xlsx")
-print(db.head())
+main_excel = pd.read_excel("Datathon Dataset.xlsx")
+main_excel['Pstng Date']=pd.to_datetime(main_excel['Pstng Date'])
+
+main_excel.to_excel('Updated Dataset.xlsx', index=False)
+print(main_excel['Pstng Date'])
